b1c6d2747f5f25cec3b741d87efdec18ed3a76da
nautics889 <cyberukr@gmail.com>
2023-05-07 20:28:40 +0300

    Fix: inappropriate HTTP-method when data is None
    Update `make_request()` of `BaseEndpoint` class.
    Change passing parameters for a `Request` object.
    Update naming slightly.

--- pyimgurapi/base_endpoint.py
+++ pyimgurapi/base_endpoint.py
@@ -1,6 +1,9 @@
 import json
+import logging
 import urllib.request
 from urllib.parse import urljoin
+
+logger = logging.getLogger(__name__)
 
 
 class BaseEndpoint:
@@ -8,14 +11,25 @@ class BaseEndpoint:
 
     def make_request(self, url_path, data=None, headers=None):
         url = urljoin(self.base_url, url_path)
-        request = urllib.request.Request(
-            url, data=json.dumps(data).encode("utf-8")
-        )
+
+        request_object_params = dict()
+        request_object_params.update(url=url)
+        if data is not None:
+            try:
+                serialized_data = json.dumps(data).encode("utf-8")
+            except TypeError:
+                logger.error("")
+                raise
+            request_object_params.update(data=serialized_data)
+
+        request = urllib.request.Request(**request_object_params)
+
         if isinstance(headers, dict):
             for header, value in headers.items():
                 request.add_header(header, value)
         request.add_header("Content-Type", "application/json")
 
         response = urllib.request.urlopen(request)
-        data = json.loads(response.read().decode("utf-8"))
-        return data
+        raw_response_data = response.read()
+        json_response_data = json.loads(raw_response_data.decode("utf-8"))
+        return json_response_data
--- pyimgurapi/image.py
+++ pyimgurapi/image.py
@@ -2,7 +2,7 @@ from pyimgurapi.base_endpoint import BaseEndpoint
 
 
 class Image(BaseEndpoint):
-    def get_image(self, image_hash, token=None):
+    def get_image(self, image_hash, access_token=None):
         url_path = f"/3/image/{image_hash}"
-        headers = {"Authorization": f"Bearer {token}"}
+        headers = {"Authorization": f"Bearer {access_token}"}
         return self.make_request(url_path, headers=headers)
