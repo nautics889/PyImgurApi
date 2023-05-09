d972766099b1afe09def4a93a956a80420424135
nautics889 <cyberukr@gmail.com>
2023-05-09 23:47:06 +0300

    Feat: Update existing image, add to favorites
    Implement `update()` and `favorite()` method for `Image` endpoint.

--- pyimgurapi/endpoints/image.py
+++ pyimgurapi/endpoints/image.py
@@ -52,5 +52,36 @@ class Image(BaseEndpoint):
 
         return self.make_request(url_path, headers=headers, method="DELETE")
 
+    def update(self, image_hash, **kwargs):
+        url_path = f"/3/image/{image_hash}"
+
+        fields = [
+            Field(field_name=field_name, value=str(kwargs.get(field_name)))
+            for field_name in ("title", "description", "album")
+            if kwargs.get(field_name) is not None
+        ]
+        file_form = MultipartForm(fields=fields)
+
+        data = bytes(file_form)
+
+        headers = {
+            "Authorization": f"Bearer {self.access_token}",
+            "Content-Type": "application/json",
+        }
+
+        return self.make_request(
+            url_path, data=data, headers=headers, method="POST"
+        )
+
+    def favorite(self, image_hash):
+        url_path = f"/3/image/{image_hash}/favorite"
+
+        headers = {
+            "Authorization": f"Bearer {self.access_token}",
+            "Content-Type": "application/json",
+        }
+
+        return self.make_request(url_path, headers=headers, method="POST")
+
     def __call__(self, image_hash):
         return self.get_image(image_hash)
