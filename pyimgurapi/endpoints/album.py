d31e30436879dde7687a7816dac105b90b1e0869
nautics889 <cyberukr@gmail.com>
2023-05-18 20:38:21 +0300

    Feat: Implement endpoints for album
    Add method for getting album.
    Add method for getting images in album.

--- pyimgurapi/endpoints/album.py
+++ pyimgurapi/endpoints/album.py
@@ -2,4 +2,25 @@ from .base_endpoint import BaseEndpoint
 
 
 class Album(BaseEndpoint):
-    pass
+    def get_album(self, album_hash):
+        url_path = f"/3/album/{album_hash}"
+
+        headers = {
+            "Content-Type": "application/json",
+        }
+        headers.update(**self.get_auth_header())
+
+        return self.make_request(url_path, headers=headers)
+
+    def album_images(self, album_hash):
+        url_path = f"/3/album/{album_hash}/images"
+
+        headers = {
+            "Content-Type": "application/json",
+        }
+        headers.update(**self.get_auth_header())
+
+        return self.make_request(url_path, headers=headers)
+
+    def __call__(self, album_hash):
+        return self.get_album(album_hash)
