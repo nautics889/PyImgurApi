328ebdea320c7874b718b988287b9b81e89c074c
nautics889 <cyberukr@gmail.com>
2023-07-09 21:18:38 +0300

    feat: update `Account` endpoint
    * add methods for managing images of an account
    * add method for getting favorite images
    * add method for getting replies

--- pyimgurapi/endpoints/account.py
+++ pyimgurapi/endpoints/account.py
@@ -29,14 +29,79 @@ class Account(BaseEndpoint):
 
         return self.make_request(url_path, headers=headers)
 
-    def gallery_favorites(self, username="me", page=None, favorite_sort=None):
+    def gallery_favorites(self, username="me", page=None, sort=None):
         url_path = f"/{self.api_version}/account/{username}/gallery_favorites"
+
+        if page is not None:
+            url_path = urljoin(f"{url_path}/", str(page))
+
+        if sort is not None:
+            url_path = urljoin(f"{url_path}/", str(sort))
+
+        headers = self.get_headers()
+
+        return self.make_request(url_path, headers=headers)
+
+    def favorites(self, username="me", page=None, favorite_sort=None):
+        url_path = f"/{self.api_version}/account/{username}/favorites"
 
         if page is not None:
             url_path = urljoin(f"{url_path}/", str(page))
 
         if favorite_sort is not None:
             url_path = urljoin(f"{url_path}/", str(favorite_sort))
+
+        headers = self.get_headers()
+
+        return self.make_request(url_path, headers=headers)
+
+    def images(self, username="me", page=None):
+        url_path = f"/{self.api_version}/account/{username}/images"
+
+        if page is not None:
+            url_path = urljoin(f"{url_path}/", str(page))
+
+        headers = self.get_headers()
+
+        return self.make_request(url_path, headers=headers)
+
+    def image(self, image_hash, username="me"):
+        url_path = f"/{self.api_version}/account/{username}/image/{image_hash}"
+
+        headers = self.get_headers()
+
+        return self.make_request(url_path, headers=headers)
+
+    def image_ids(self, username="me", page=None):
+        url_path = f"/{self.api_version}/account/{username}/images/ids/{page}"
+
+        if page is not None:
+            url_path = urljoin(f"{url_path}/", str(page))
+
+        headers = self.get_headers()
+
+        return self.make_request(url_path, headers=headers)
+
+    def image_count(self, username="me"):
+        url_path = f"/{self.api_version}/account/{username}/images/count"
+
+        headers = self.get_headers()
+
+        return self.make_request(url_path, headers=headers)
+
+    def image_delete(self, delete_hash, username="me"):
+        url_path = (
+            f"/{self.api_version}" f"/account/{username}/image/{delete_hash}"
+        )
+
+        headers = self.get_headers()
+
+        return self.make_request(url_path, headers=headers, method="DELETE")
+
+    def replies(self, username="me"):
+        url_path = (
+            f"/{self.api_version}/account/{username}" f"/notifications/replies"
+        )
 
         headers = self.get_headers()
 
